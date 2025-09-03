40665eb33b910e76d81f9326ba72fbbf155fcbcd
Taner Saydam <taner_and_you@hotmail.com>
2025-09-03 16:17:48 +0300

    Controller için Validation'ı otomatikleştirelim

---
+++ DotnetLibraries.WebAPI/Attributes/ValidateAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using DotnetLibraries.FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotnetLibraries.WebAPI.Attributes;
+
+public sealed class ValidateAttribute : Attribute, IActionFilter
+{
+    public void OnActionExecuted(ActionExecutedContext context) { }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        var body = context.ActionArguments.FirstOrDefault().Value;
+        if (body is null) return;
+
+        var bodyType = body.GetType();
+        var validatorBaseType = typeof(AbstractValidator<>).MakeGenericType(bodyType);
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var types = assembly.GetTypes()
+            .Where(i => i.IsClass
+            && !i.IsAbstract
+            && validatorBaseType.IsAssignableFrom(i)
+            );
+
+        List<ValidationResult> results = new();
+        foreach (var type in types)
+        {
+            var instance = Activator.CreateInstance(type);
+            if (instance is null) continue;
+
+            MethodInfo? methodInfo = type.GetMethod("Validate");
+            if (methodInfo is null) continue;
+            ValidationResult result = (ValidationResult)methodInfo.Invoke(instance, [body])!;
+            if (!result.IsValid) results.Add(result);
+        }
+
+        if (results.Any())
+        {
+            context.Result = new ObjectResult(results.SelectMany(i => i.Errors).Distinct())
+            {
+                StatusCode = 422
+            };
+
+            return;
+        }
+    }
+}
--- DotnetLibraries.WebAPI/Controllers/ProductsController.cs
+++ DotnetLibraries.WebAPI/Controllers/ProductsController.cs
@@ -1,25 +1,17 @@
-﻿using DotnetLibraries.FluentValidation;
+﻿using DotnetLibraries.WebAPI.Attributes;
 using DotnetLibraries.WebAPI.Dtos;
-using DotnetLibraries.WebAPI.Validators;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetLibraries.WebAPI.Controllers;
 
 [ApiController]
 [Route("/api/products")]
+[Validate]
 public sealed class ProductsController : ControllerBase
 {
     [HttpPost]
     public IActionResult Create(CreateProductDto request)
     {
-        CreateProductValidator validator = new();
-        ValidationResult validationResult = validator.Validate(request);
-        if (!validationResult.IsValid)
-        {
-            var message = validationResult.Errors;
-            return BadRequest(message);
-        }
-
         return Ok(new { Message = "Create product is successful" });
     }
 }
