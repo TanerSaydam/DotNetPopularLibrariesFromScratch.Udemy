69ecca6cd57296628876286780da52c4a70a5bde
Taner Saydam <taner_and_you@hotmail.com>
2025-09-03 15:25:31 +0300

    RuleFor da expression değerini compile yapıp kontrol için saklayalım

--- DotnetLibraries.FluentValidation/AbstractValidator.cs
+++ DotnetLibraries.FluentValidation/AbstractValidator.cs
@@ -4,8 +4,13 @@ namespace DotnetLibraries.FluentValidation;
 public abstract class AbstractValidator<TEntity>
     where TEntity : class
 {
+    private readonly List<Func<TEntity, object>> _getters = new();
     public IRuleBuilder RuleFor<TProperty>(Expression<Func<TEntity, TProperty>> expression)
     {
+        var compile = expression.Compile();
+        Func<TEntity, object> getter = x => compile(x)!;
+        _getters.Add(getter);
+
         return new RuleBuilder();
     }
 
--- DotnetLibraries.WebAPI/Validators/CreateProductValidator.cs
+++ DotnetLibraries.WebAPI/Validators/CreateProductValidator.cs
@@ -8,6 +8,6 @@ public sealed class CreateProductValidator : AbstractValidator<CreateProductDto>
     public CreateProductValidator()
     {
         RuleFor(p => p.Name).NotEmpty();//.WithMessage("This is a custom message for name");
-        //RuleFor(p => p.Price).GreaterThan(0).WithMessage("This is a custom message for price");
+        RuleFor(p => p.Price).NotEmpty();//.GreaterThan(0).WithMessage("This is a custom message for price");
     }
 } //lambda expression
