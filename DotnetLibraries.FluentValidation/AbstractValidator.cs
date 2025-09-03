653d794d64a1e0ca2651d0721c9540309bd46f81
Taner Saydam <taner_and_you@hotmail.com>
2025-09-03 14:33:53 +0300

    RuleFor

--- DotnetLibraries.FluentValidation/AbstractValidator.cs
+++ DotnetLibraries.FluentValidation/AbstractValidator.cs
@@ -1,5 +1,10 @@
-﻿namespace DotnetLibraries.FluentValidation;
+﻿using System.Linq.Expressions;
+
+namespace DotnetLibraries.FluentValidation;
 public abstract class AbstractValidator<TEntity>
     where TEntity : class
 {
+    public void RuleFor<TProperty>(Expression<Func<TEntity, TProperty>> expression)
+    {
+    }
 }
--- DotnetLibraries.WebAPI/Controllers/ProductsController.cs
+++ DotnetLibraries.WebAPI/Controllers/ProductsController.cs
@@ -1,6 +1,4 @@
 ﻿using DotnetLibraries.WebAPI.Dtos;
-using DotnetLibraries.WebAPI.Validators;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetLibraries.WebAPI.Controllers;
@@ -12,13 +10,13 @@ public sealed class ProductsController : ControllerBase
     [HttpPost]
     public IActionResult Create(CreateProductDto request)
     {
-        CreateProductValidator validator = new();
-        ValidationResult validationResult = validator.Validate(request);
-        if (!validationResult.IsValid)
-        {
-            var message = validationResult.Errors;
-            return BadRequest(message);
-        }
+        //CreateProductValidator validator = new();
+        //ValidationResult validationResult = validator.Validate(request);
+        //if (!validationResult.IsValid)
+        //{
+        //    var message = validationResult.Errors;
+        //    return BadRequest(message);
+        //}
 
         return Ok(new { Message = "Create product is successful" });
     }
