7868db0230fdda7da087e29612bf8562a17b2c3b
Taner Saydam <taner_and_you@hotmail.com>
2025-09-04 07:18:37 +0300

    Property eşlemelerini cache'leyelim

--- DotnetLibraries.Mapster/Extensions.cs
+++ DotnetLibraries.Mapster/Extensions.cs
@@ -1,8 +1,41 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
 
 namespace DotnetLibraries.Mapster;
 public static class Extensions
 {
+    private static readonly ConcurrentDictionary<(Type src, Type dest), Action<object, object>> _cache = new();
+
+    private static Action<object, object> Get(Type src, Type dest)
+        => _cache.GetOrAdd((src, dest), Build);
+
+    private static Action<object, object> Build((Type src, Type dest) key)
+    {
+        var srcProperties = key.src.GetProperties(
+            BindingFlags.Instance | BindingFlags.Public);
+        var destProperties = key.dest.GetProperties(
+             BindingFlags.Instance | BindingFlags.Public)
+            .Where(p => p.CanWrite)
+            .ToDictionary(p => p.Name);
+
+        var pairs = new List<(PropertyInfo Src, PropertyInfo dest)>();
+        foreach (var srcProp in srcProperties)
+        {
+            if (!destProperties.TryGetValue(srcProp.Name, out var dp)) continue;
+            if (!dp.PropertyType.IsAssignableFrom(srcProp.PropertyType)) continue;
+
+            pairs.Add((srcProp, dp));
+        }
+
+        return (srcObj, destObj) =>
+        {
+            foreach (var (sp, dp) in pairs)
+            {
+                var value = sp.GetValue(srcObj);
+                dp.SetValue(destObj, value);
+            }
+        };
+    }
     private static TEntity MapTo<TEntity>(object src, TEntity instance) where TEntity : class, new()
     {
         var srcProperties = src.GetType().GetProperties(
@@ -27,13 +60,13 @@ public static class Extensions
         where TEntity : class, new()
     {
         var instance = new TEntity();
-        return MapTo(src, instance);
+        Get(src.GetType(), instance.GetType())(src, instance);
+        return instance;
     }
-
     public static TEntity Adapt<TEntity>(this object src, TEntity dest)
         where TEntity : class, new()
     {
-
-        return MapTo(src, dest);
+        Get(src.GetType(), dest.GetType())(src, dest);
+        return dest;
     }
 }
